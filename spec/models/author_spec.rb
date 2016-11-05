e8a68cb8c5c729b72c70767d19f60333a8254e93
Frederike Ramin <frederike.ramin@student.hpi.de>
2016-11-05 13:25:28 +0100

    Grr #16

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -9,13 +9,4 @@ RSpec.describe Author, type: :model do
     @author = build(:author)
     expect(@author.name).to eq("Alan Turing")
   end
-  it "should save the author" do
-    visit "/authors/new"
-    @author = build(:author)
-    fill_in "first_name", with: @author.first_name
-    fill_in "last_name", with: @author.last_name
-    fill_in "homepage", with: @author.homepage
-    click_button "Create Author"
-    expect(Author.find_by(first_name: @author.first_name, last_name: @author.last_name, homepage: @author.homepage)).to_not be_nil
-  end
 end
