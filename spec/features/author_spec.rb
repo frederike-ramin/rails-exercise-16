9c2cf40bb178f5c5de844c581d78becbc9dde450
Frederike Ramin <frederike.ramin@student.hpi.de>
2016-11-05 16:42:09 +0100

    Next try...

--- spec/features/author_spec.rb
+++ spec/features/author_spec.rb
@@ -2,12 +2,11 @@ require 'rails_helper'
 
 describe "Authors page", :type => :feature do
     it "should save the author" do
-        visit "/authors/new"
-        @author = build(:author)
-        fill_in first_name, with: @author.first_name
-        fill_in last_name, with: @author.last_name
-        fill_in homepage, with: @author.homepage
-        click_button "Create Author"
-        expect(Author.find_by(first_name: @author.first_name, last_name: @author.last_name, homepage: @author.homepage)).to_not be_nil
+        visit '/authors/new'
+        fill_in(first_name, with: 'Alan')
+        fill_in(last_name, with: 'Turing')
+        fill_in(homepage, with: 'http://wikipedia.org/Alan_Turing')
+        click_button 'Create Author'
+        expect(Author.find_by(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')).to_not be_nil
     end
 end
