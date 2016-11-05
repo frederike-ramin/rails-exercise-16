03493dc4b07c60da85b7300770f09ab04ee9b533
Frederike Ramin <frederike.ramin@student.hpi.de>
2016-11-05 11:27:42 +0100

    Test author creation

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,7 +1,22 @@
 class AuthorsController < ApplicationController
     def new
     end
-    def create
-        render plain: params[:author].inspect
+=begin
+        def create
+        render plain: params[:article].inspect
+        #@author = Author.new(author_params)
+
+        #@author.save
+        #redirect_to @author
     end
+
+    private
+          def author_params
+            params.require(:author).permit(:first_name, :last_name, :homepage)
+          end
+
+    def show
+        @author = Author.find(params[:id])
+    end
+=end
 end
