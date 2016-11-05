7e9953acfff5f7b15a69302b1f0a840fee9b51fe
Frederike Ramin <frederike.ramin@student.hpi.de>
2016-11-05 13:30:30 +0100

    Another error..Hype!

--- spec/features/author_spec.rb
+++ spec/features/author_spec.rb
@@ -4,9 +4,9 @@ describe "Authors page", :type => :feature do
     it "should save the author" do
         visit "/authors/new"
         @author = build(:author)
-        fill_in "first_name", with: @author.first_name
-        fill_in "last_name", with: @author.last_name
-        fill_in "homepage", with: @author.homepage
+        fill_in first_name, with: @author.first_name
+        fill_in last_name, with: @author.last_name
+        fill_in homepage, with: @author.homepage
         click_button "Create Author"
         expect(Author.find_by(first_name: @author.first_name, last_name: @author.last_name, homepage: @author.homepage)).to_not be_nil
     end
