e24a2c780eb7f054baef449d3e1bc9298181220d
Frederike Ramin <frederike.ramin@student.hpi.de>
2016-11-05 13:01:59 +0100

    Fix Test for #16

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -11,6 +11,11 @@ RSpec.describe Author, type: :model do
   end
   it "should save the author" do
     @author = build(:author)
+    visit "/authors/new"
+    fill_in "first_name" with: @author.first_name
+    fill_in "last_name" with: @author.last_name
+    fill_in "homepage" with: @author.homepage
+    click_button "Create Author"
     expect(Author.find_by(first_name: @author.first_name, last_name: @author.last_name, homepage: @author.homepage)).to_not be_nil
   end
 end
