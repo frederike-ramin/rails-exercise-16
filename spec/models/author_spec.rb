1d2800ba43d4571e11165eaa1ada639e41537ccb
Frederike Ramin <frederike.ramin@student.hpi.de>
2016-11-05 12:39:38 +0100

    Failing test for #16

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -9,4 +9,8 @@ RSpec.describe Author, type: :model do
     @author = build(:author)
     expect(@author.name).to eq("Alan Turing")
   end
+  it "should save the author" do
+    @author = build(:author)
+    expect(Author.find_by(first_name: @author.first_name, last_name: @author.last_name, homepage: @author.homepage)).to_not be_nil
+  end
 end
